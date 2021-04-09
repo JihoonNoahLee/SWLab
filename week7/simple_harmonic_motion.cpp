ba69a942adbc2002ebb6e357efb0877cc0770123
Lee Jihoon <jihoolee@student.42SEOUL.kr>
2021-04-09 13:47:34 +0900

    week7 start

---
+++ week7/simple_harmonic_motion.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+#define PI 3.141592
+
+using namespace std;
+
+int	main(void)
+{
+	float	x2, x1, x0;
+	float	dt, k;
+
+	k = 10;
+	dt = (2 * PI / 10.) / 20.;
+
+
+	return (0);
+}
